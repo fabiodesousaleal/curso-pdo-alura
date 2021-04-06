c8d002be84200d1518ec3591618357300eee61da
fabiodesousaleal <fsl2@msn.com>
2021-04-06 17:48:26 -0300

    Criado CRUD orientado a Objeto, repositorio e conexao

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 
-$caminhoBanco = __DIR__ . '/banco.sqlite3';
-$pdo = new PDO('sqlite:' . $caminhoBanco);
 
-echo 'Conectado'.PHP_EOL;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+
+$pdo = ConnectionCreator::connectionCreator();
 $pdo->exec( 'CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
